949cf9866d078b267860a46f34a828f7852bf551
jccari <jhoelccari.23@gmail.com>
2021-07-02 02:22:12 -0500

    terran results

---
+++ terran-results/html/results/results_summary_json.js
@@ -0,0 +1,165 @@
+var resultsTXTSize = 31;
+var lastUpdateTime = '2021-07-01 [02:48:06]';
+var maps = ["(2)Benzene.scx","(2)Destination.scx","(2)HeartbreakRidge.scx","(3)Aztec.scx","(3)TauCross.scx","(4)Andromeda.scx","(4)CircuitBreaker.scx","(4)EmpireoftheSun.scm","(4)Fortress.scx","(4)Python.scx"];
+var resultsSummary = [
+  {
+    "BotName": "UAlbertaBot",
+    "Rank": 0,
+    "Race": "Terran",
+    "BWAPIVersion": "4.4.0",
+    "Games": 248,
+    "Wins": 133,
+    "Losses": 115,
+    "Score": "53.63",
+    "ELO": 1188,
+    "AvgTime": 18763,
+    "WallTime": 93,
+    "Hour": 10,
+    "Crash": 32,
+    "Timeout": 0,
+    "resultPairs": [
+      {
+        "Opponent": "UAlbertaBot",
+        "Wins": 0,
+        "Games": 0
+      },
+      {
+        "Opponent": "UABIntegral",
+        "Wins": 133,
+        "Games": 248
+      }
+    ],
+    "mapResults": [
+      {
+        "Map": "Benzene",
+        "Wins": 14,
+        "Games": 25
+      },
+      {
+        "Map": "Destination",
+        "Wins": 8,
+        "Games": 25
+      },
+      {
+        "Map": "HeartbreakRidge",
+        "Wins": 14,
+        "Games": 25
+      },
+      {
+        "Map": "Aztec",
+        "Wins": 12,
+        "Games": 25
+      },
+      {
+        "Map": "TauCross",
+        "Wins": 10,
+        "Games": 24
+      },
+      {
+        "Map": "Andromeda",
+        "Wins": 14,
+        "Games": 24
+      },
+      {
+        "Map": "CircuitBreaker",
+        "Wins": 17,
+        "Games": 25
+      },
+      {
+        "Map": "EmpireoftheSun",
+        "Wins": 13,
+        "Games": 25
+      },
+      {
+        "Map": "Fortress",
+        "Wins": 13,
+        "Games": 25
+      },
+      {
+        "Map": "Python",
+        "Wins": 18,
+        "Games": 25
+      }
+    ]
+  },
+  {
+    "BotName": "UABIntegral",
+    "Rank": 1,
+    "Race": "Terran",
+    "BWAPIVersion": "4.4.0",
+    "Games": 248,
+    "Wins": 115,
+    "Losses": 133,
+    "Score": "46.37",
+    "ELO": 1211,
+    "AvgTime": 18763,
+    "WallTime": 93,
+    "Hour": 10,
+    "Crash": 37,
+    "Timeout": 0,
+    "resultPairs": [
+      {
+        "Opponent": "UAlbertaBot",
+        "Wins": 115,
+        "Games": 248
+      },
+      {
+        "Opponent": "UABIntegral",
+        "Wins": 0,
+        "Games": 0
+      }
+    ],
+    "mapResults": [
+      {
+        "Map": "Benzene",
+        "Wins": 11,
+        "Games": 25
+      },
+      {
+        "Map": "Destination",
+        "Wins": 17,
+        "Games": 25
+      },
+      {
+        "Map": "HeartbreakRidge",
+        "Wins": 11,
+        "Games": 25
+      },
+      {
+        "Map": "Aztec",
+        "Wins": 13,
+        "Games": 25
+      },
+      {
+        "Map": "TauCross",
+        "Wins": 14,
+        "Games": 24
+      },
+      {
+        "Map": "Andromeda",
+        "Wins": 10,
+        "Games": 24
+      },
+      {
+        "Map": "CircuitBreaker",
+        "Wins": 8,
+        "Games": 25
+      },
+      {
+        "Map": "EmpireoftheSun",
+        "Wins": 12,
+        "Games": 25
+      },
+      {
+        "Map": "Fortress",
+        "Wins": 12,
+        "Games": 25
+      },
+      {
+        "Map": "Python",
+        "Wins": 7,
+        "Games": 25
+      }
+    ]
+  }
+];
